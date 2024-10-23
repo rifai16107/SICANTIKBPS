1135a645d9d813ea7c8877fad645c05fb26aa222
rifai16107 <rifai3301e@gmail.com>
2024-10-23 08:44:53 +0700

    coba

--- lib/homescreen_menu/sensus_ekonomi/sensus_ekonomi_content.dart
+++ lib/homescreen_menu/sensus_ekonomi/sensus_ekonomi_content.dart
@@ -665,7 +665,7 @@ class _SensusEkonomiContentState extends State<SensusEkonomiContent> {
                                   width: screenWidth * 0.89,
                                   //height: screenHeight * 0.24,
                                   child: const Text(
-                                    "Enjoy The Song Bapak/Ibu",
+                                    "Enjoy The Song....",
                                     textAlign: TextAlign.justify,
                                     style: TextStyle(
                                         fontFamily: 'Robo',
