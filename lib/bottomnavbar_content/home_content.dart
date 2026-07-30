d8bf71c2688e8bca7b9563b4129731958ce173b0
rifai16107 <rifai3301@ggmail.com>
2026-07-30 16:51:06 +0700

    modified:   lib/bottomnavbar_content/home_content.dart 	new file:   lib/carousel/carousel_statis.dart

--- lib/bottomnavbar_content/home_content.dart
+++ lib/bottomnavbar_content/home_content.dart
@@ -7,6 +7,7 @@ import 'package:bps_cilacap/carousel/carousel_4.dart';
 import 'package:bps_cilacap/carousel/carousel_5.dart';
 import 'package:bps_cilacap/carousel/carousel_6.dart';
 import 'package:bps_cilacap/carousel/carousel_7.dart';
+import 'package:bps_cilacap/carousel/carousel_statis.dart';
 import 'package:bps_cilacap/homescreen_menu/kemiskinan_content.dart';
 import 'package:bps_cilacap/homescreen_menu/ketimpangan_content.dart';
 import 'package:bps_cilacap/homescreen_menu/pengangguran_content.dart';
@@ -62,6 +63,7 @@ class _HomeContentState extends State<HomeContent> {
     const carouselSlider5(),
     const carouselSlider6(),
     const carouselSlider7(),
+    const carouselStatis(),
   ];
   final CarouselSliderController _controller = CarouselSliderController();
 
