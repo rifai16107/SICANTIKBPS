02729cec4142b59ba7dc4322b6e99468b54c4a63
rifai16107 <rifai3301@gmail.com>
2026-08-04 19:54:49 +0700

    modified:   lib/homescreen_menu/hasil_skd/skdtriwulanan_a.dart

--- lib/homescreen_menu/hasil_skd/skdtriwulanan_a.dart
+++ lib/homescreen_menu/hasil_skd/skdtriwulanan_a.dart
@@ -274,7 +274,7 @@ class _SkdTriwulananAState extends State<SkdTriwulananA> {
                                 width: screenWidth * 0.25,
                                 height: screenHeight * 0.08,
                                 child: Text(
-                                  'IPAK',
+                                  'IPAAK',
                                   style: const TextStyle(
                                     color: Colors.black,
                                     fontWeight: FontWeight.bold,
